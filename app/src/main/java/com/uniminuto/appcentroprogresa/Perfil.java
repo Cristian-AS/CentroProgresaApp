febf51481400ac24339183aca212a082124fc140
Camila Acosta Velasquez <camila.acosta-v@uniminuto.edu.co>
2023-06-21 22:53:50 -0500

    Interfaces listas de perfil y clasificacion

--- app/src/main/java/com/uniminuto/appcentroprogresa/Perfil.java
+++ app/src/main/java/com/uniminuto/appcentroprogresa/Perfil.java
@@ -1,14 +1,52 @@
 package com.uniminuto.appcentroprogresa;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class Perfil extends AppCompatActivity {
+
+    final ProcessFragment processFragment = new ProcessFragment();
+     final NotificationsFragment notificationsFragment = new NotificationsFragment();
+    final ProfileFragment profileFragment = new ProfileFragment();
+    final SettingsFragment settingsFragment = new SettingsFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+
+        BottomNavigationView navigation = findViewById(R.id.bottom_navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        // lo primero que se carga
+        loadFragment(profileFragment); 
+    }
+    // Para pasar de fragmentos
+    private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = item -> {
+        int itemId = item.getItemId();
+        if (itemId == R.id.processFragment) {
+            loadFragment(processFragment);
+            return true;
+        } else if (itemId == R.id.notificationsFragment) {
+            loadFragment(notificationsFragment);
+            return true;
+        } else if (itemId == R.id.profileFragment) {
+            loadFragment(profileFragment);
+            return true;
+        } else if (itemId == R.id.settingsFragment) {
+            loadFragment(settingsFragment);
+            return true;
+        }
+        return false;
+    };
+
+    public void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.commit();
     }
 }
